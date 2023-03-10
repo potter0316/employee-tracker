e25d12eec4362e47418ba12011e2be9f2d5754fa
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-03-09 20:03:55 -0600

    working on inquirer prompts

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 // Import and require mysql2
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 // import and requre inquirer@8.2.4
-const inquirer = require('inquirer');
-const cTable = require('console.table');
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,7 +13,55 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
-const db = require('./config/connection')
+const db = require("./config/db");
+
+db.promise()
+  .query("Select")
+  .then(([rows, fields]) => {
+    console.log(rows);
+  })
+  .catch(console.log)
+  .then(() => confirm.end());
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("connected" + db.threadId);
+  afterConnection();
+});
+
+afterConnection = () => {
+  promptUser();
+};
+
+inquirer.prompt([
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "choices",
+      message: "What would you like to do?",
+      Choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+    },
+  ]),
+]);
+
+addDepartment = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'addDepartment',
+      message: "Name of new Department"
+    }
+  ])
+}
 
 app.use((req, res) => {
   res.status(404).end();
